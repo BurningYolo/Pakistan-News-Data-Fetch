32a70533c14b4141c31c0b712ec2842781b50a78
BurningYolo <hishamzahid123@gmail.com>
2024-02-19 10:52:20 +0500

    working... Fetched Articles

--- ary_news_data/ary_news_article_data_fetch.js
+++ ary_news_data/ary_news_article_data_fetch.js
@@ -1,15 +1,12 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-var ary_article_arr= []; // ARY ARTICLE STORER  
+var ary_article_arr = []; // ARY ARTICLE STORER  
 
 async function aryNewsArticleFetch(href) {
-
-
-
-
-  for (var i = 0; i < 10; i++)  // IF GET VALUE OF ALL ARTICLES NEED TO CHANGE 10 to Length of href arr .... gotten from param. 
-  {    
-    try {
+  try {
+    for (var i = 0; i < 10; i++) {    
+      ary_article_arr[i] = " "; // Initialize as an empty string for each article
+      
       const url = href[i];
       console.log(url);
       const response = await axios.get(url);
@@ -17,33 +14,26 @@ async function aryNewsArticleFetch(href) {
       if (response.status === 200) {
         const $ = cheerio.load(response.data);
 
-        // Select only <p> tags that contain text
-
-
         const paragraphs = $('.tdb-block-inner p').filter(function () {
           return $(this).text().trim().length > 0;
         });
-       
-        // Extract and print the text content of selected <p> tags
-
 
         paragraphs.each((index, element) => {
           const paragraphText = $(element).text();
-          ary_article_arr[i] += paragraphText; 
+          ary_article_arr[i] += paragraphText;
         });
 
-        console.log(ary_article_arr); 
+        console.log(ary_article_arr);
       } else {
         console.log('Failed to retrieve the webpage. Status code:', response.status);
       }
 
       await new Promise(resolve => setTimeout(resolve, 1000));
-    } catch (error) {
-      console.error('Failed to retrieve the webpage.', error.message);
     }
+    require('../geo_news_data/geo_news_heading_link_fetch').geoNewsHeadingFetch(); 
+  } catch (error) {
+    console.error('Failed to retrieve the webpage.', error.message);
   }
-  require('../geo_news_data/geo_news_heading_link_fetch').geoNewsHeadingFetch(); 
-
 }
 
-module.exports = { aryNewsArticleFetch , ary_article_arr };
+module.exports = { aryNewsArticleFetch, ary_article_arr };
--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 
+const fs = require('fs');
 const {aryNewsHeadingFetch , ary_title_arr} = require('./ary_news_data/ary_news_heading_link_fetch')
 const {geoNewsHeadingFetch , geo_title_arr} = require('./geo_news_data/geo_news_heading_link_fetch')
 const {dunyaNewsHeadingFetch , dunya_title_arr} = require('./dunya_news_data/dunya_news_heading_link_fetch')
 const {bolNewsHeadingFetch , bol_title_arr} = require('./bol_news_data/bol_news_heading_link_fetch')
 const {humNewsHeadingFetch , hum_title_arr} = require('./hum_news_data/hum_news_heading_link_fetch')
-const fs = require('fs');
+const {aryNewsArticleFetch , ary_article_arr} = require('./ary_news_data/ary_news_article_data_fetch')
+const {geoNewsArticleFetch , geo_article_arr} = require('./geo_news_data/geo_news_article_data_fetch')
+const {duynaNewsArticleFetch , dunya_article_arr} = require('./dunya_news_data/dunya_news_article_data_fetch')
+const {bolNewsArticleFetch , bol_article_arr} = require('./bol_news_data/bol_news_article_data_fetch')
+const {humNewsArticleFetch , hum_article_arr} = require('./hum_news_data/hum_news_article_data_fetch')
+
+
+
+
+
 var all_titles=[]; 
+var all_articles=[]; 
 
 
 function start_fetching_ary()
@@ -35,13 +46,40 @@ function gather_articles_heading()
       console.log('Titles have been written to', filePath);
     }
   });
+  gather_articles(); 
+
+
 }
 
-start_fetching_ary();
+
+
+function gather_articles()
+{
+    // Assuming ary_title_arr, geo_title_arr, dunya_title_arr, bol_title_arr, hum_title_arr are arrays
+   all_articles = ary_title_arr.concat(ary_article_arr , geo_article_arr , hum_article_arr , bol_article_arr , dunya_article_arr);
+
+  const filePath = 'ARTICLE.txt';
+
+  // Create a string with each title on a separate line
+  const StringG = all_articles.join('\n\n');
+
+  // Write the string to a text file
+  fs.writeFile(filePath, StringG, 'utf-8', (err) => {
+    if (err) {
+      console.error('Error writing to file:', err);
+    } else {
+      console.log('Titles have been written to', filePath);
+    }
+  });
+
+
+}
+
+start_fetching_ary(); // ary --> geo --> bol --> dunya --> hum 
 
 
 module.exports = { gather_articles_heading };
 
+ 
 
 
-// ary --> geo --> bol --> dunya --> hum 
